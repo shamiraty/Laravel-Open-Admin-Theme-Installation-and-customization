5365f7eebce8b7c9a1d15b42351b4e2ac19ead36
shamiraty <sashashamsia@gmail.com>
2023-10-11 21:32:14 +0300

    first commit

---
+++ app/Admin/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Admin\Controllers\UserController;
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+],
+
+    function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('home');
+
+   $router->resource('students',\App\Admin\Controllers\StudentController::class);
+
+   $router->resource('users', UserController::class);
+
+});
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use OpenAdmin\Admin\Config\Config;
+use Illuminate\Support\Facades\Schema;
+
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+
+    public function boot()
+    {
+        $table = config('admin.extensions.config.table', 'admin_config');
+        if (Schema::hasTable($table)) {
+            Config::load();
+        }
+    }
+}
